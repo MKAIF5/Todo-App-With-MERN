d1c891c87bacd6dcc0b852dcb90464814c9cf947
mohammed kaif <mkaif555123@gmail.com>
2025-01-03 08:15:13 +0500

    backend edit and delete work completed

--- Back-End/index.mjs
+++ Back-End/index.mjs
@@ -9,16 +9,28 @@ app.use(express.json())
 
 // Todo Get
 app.get('/all-todo', (request, response) => {
-    response.send(todos)
+
+    const message = !todos.length ? "Todos Is Empty" : "This Is All Todos"
+
+    response.send({
+        data: todos,
+        message: message
+    })
 });
 
 // Todo Add
 app.post('/add-todo', (request, response) => {
-    todos.push({
-        todo: request.body.todo,
-        id: String(new Date().getTime())
-    })
-    response.send("todo added");
+
+    const obj = {
+        todoContent: request.body.todoContent,
+        id: String(new Date().getTime()),
+    }
+
+    todos.push(obj)
+    response.send({
+        message: "todo added",
+        data: obj
+    });
 })
 
 // Todo Edit
@@ -26,19 +38,49 @@ app.patch('/edit-todo/:id', (request, response) => {
 
     const id = request.params.id;
 
+    let isFound = false
     for (let i = 0; i < todos.length; i++) {
-
         if (todos[i].id == id) {
-            todos[i].todo = request.body.todo;
+            todos[i].todoContent = request.body.todoContent;
+            isFound = true
+            break;
         }
 
-        response.status(201).send("Todo Added Successfully");
     };
+
+    if (isFound) {
+        response.status(201).send({
+            data: {
+                todoContent: request.body.todoContent,
+                id: id
+            }, message: "Todo Added Successfully"
+        })
+    } else {
+        response.status(200).send({ data: null, message: "Todo Not Found" });
+    }
 
 });
 
 // Todo Delete
 app.delete('/delete-todo/:id', (request, response) => {
+
+    const id = request.params.id;
+
+    let isFound = false
+    for (let i = 0; i < todos.length; i++) {
+        if (todos[i].id == id) {
+            todos.splice(i, 1);
+            isFound = true
+            break;
+        }
+
+    };
+
+    if (isFound) {
+        response.status(201).send({ message: "Todo Delete Successfully" })
+    } else {
+        response.status(200).send({ data: null, message: "Todo Not Found" });
+    }
 
 });
 
